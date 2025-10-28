103bf2a6e8f838969b8a223fc2e65e4c52b2464c
ngminhduc1412007-dotcom <ngminhduc1412007@gmail.com>
2025-10-29 00:52:56 +0700

    Add files via upload

--- bai luyen tap 8.c
+++ bai luyen tap 8.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    float kWh, tienBacThang, phuPhi, tienDien;
+    float soDien, tienBacThang, phuPhi, tienDien;
     int cacLoaiHoSuDungDien;
 
     printf("nhap so dien tieu thu: ");
-    scanf("%f", &kWh);
+    scanf("%f", &soDien);
 
     printf("nhap loai ho su dung dien : ");
     scanf("%d", &cacLoaiHoSuDungDien);
 
-    if (kWh <= 0) {
+    if (soDien <= 0) {
         printf("so dien khong hop le \n");
         return 0;
     }
-    else if (kWh <= 50) {
-        tienBacThang = kWh * 1500;
+    else if (soDien <= 50) {
+        tienBacThang = soDien * 1500;
     }
-    else if (kWh <= 100) {
-        tienBacThang = 50 * 1500 + (kWh - 50) * 2000;
+    else if (soDien <= 100) {
+        tienBacThang = 50 * 1500 + (soDien - 50) * 2000;
     }
-    else if (kWh <= 200) {
-        tienBacThang = 50 * 1500 + 50 * 2000 + (kWh - 100) * 2500;
+    else if (soDien <= 200) {
+        tienBacThang = 50 * 1500 + 50 * 2000 + (soDien - 100) * 2500;
     }
     else {
-        tienBacThang = 50 * 1500 + 50 * 2000 + 100 * 2500 + (kWh - 200) * 3000;
+        tienBacThang = 50 * 1500 + 50 * 2000 + 100 * 2500 + (soDien - 200) * 3000;
     }
 
     switch (cacLoaiHoSuDungDien) {
@@ -44,7 +44,7 @@ int main() {
 
     tienDien = tienBacThang + phuPhi;
 
-    printf("so dien tieu thu: %f kWh\n", kWh);
+    printf("so dien tieu thu: %f kWh\n",soDien);
     printf("tien bac thang: %f \n", tienBacThang);
     printf("phu phi: %f \n", phuPhi);
     printf("tong tien dien cua thang la: %f \n", tienDien);
